9b180dd40b3066d0d144fb02cbc90107d5c5b848
Aaron Emery <aemery81@gmail.com>
2021-11-03 10:04:27 -0700

    giphy hook init

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -7,19 +7,25 @@ const Gif = ({ className }) => {
   let image;
 
   useEffect(() => {
-    fetch(`https://api.giphy.com/v1/gifs/random?&limit=1&tag=scream&api_key=${process.env.GATSBY_GIPHY_API}`)
-    .then(response => response.json())
-    .then(responseData => setResults(responseData.data))
-    .then(image = response.results.images.fixed_height_downsampled.url)
-    .catch(error => {
-        console.log('Error fetching and parsing data', error)
-      })
+    // fetch(`https://api.giphy.com/v1/gifs/random?&limit=1&tag=scream&api_key=${process.env.GATSBY_GIPHY_API}`)
+    // .then(response => response.json())
+    // .then(responseData => setResults(responseData.data))
+    // .then(image = response.results.images.fixed_height_downsampled.url)
+    // .catch(error => {
+    //     console.log('Error fetching and parsing data', error)
+    //   })
+    if(Date.parse(localStorage.getItem('date')) > new Date()) {
+      localStorage.setItem('date', new Date())
+      localStorage.setItem('updated', 'true')
+    } else {
+      localStorage.setItem('updated', 'false')
+    }
   },[])
 
   console.log(`results: ${JSON.stringify(results)}`);
 
   return (
-    <img src={results.images.fixed_height_downsampled.url} alt={results.title} className={className} />
+    <img src={'results.images.fixed_height_downsampled.url'} alt={'results.title'} className={className} />
   )
 }
 
