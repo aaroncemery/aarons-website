faf34164271f89451b7fde93d9f440c503fac346
Aaron Emery <aemery81@gmail.com>
2021-11-03 08:02:25 -0700

    fixing giphy api

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from 'react'
 const Gif = ({ className }) => {
   const [results, setResults] = useState([])
   // const { title } = results
+  let image;
 
   useEffect(() => {
     fetch(`https://api.giphy.com/v1/gifs/random?&limit=1&tag=scream&api_key=${process.env.GATSBY_GIPHY_API}`)
     .then(response => response.json())
     .then(responseData => setResults(responseData.data))
+    .then(image = response.results.images.fixed_height_downsampled.url)
     .catch(error => {
         console.log('Error fetching and parsing data', error)
       })
